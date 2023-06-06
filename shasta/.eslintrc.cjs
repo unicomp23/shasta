fd99ba726df016533135e1e604850a2b72dfd9b0
John Davis <john.davis@airtime.com>
2023-06-06 11:24:51 -0700

    lint attempt #2

---
+++ shasta/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    ignorePatterns: ['test/**/*', 'src/legacy/**/*', 'src/**/*.js'],
+    env: { browser: true },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+    }
+};
