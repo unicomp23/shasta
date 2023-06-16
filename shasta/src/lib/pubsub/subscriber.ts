543aae82664645b8a9b6f6f16fce67802098de41
John Davis <john.davis@airtime.com>
2023-06-16 15:20:35 -0700

    wip, subscriber

--- shasta/src/lib/pubsub/subscriber.ts
+++ shasta/src/lib/pubsub/subscriber.ts
@@ -1,66 +1,60 @@
-import Redis, { RedisOptions } from 'ioredis';
-import { TagData, TagDataEnvelope, TagDataSnapshot } from '../../../submodules/src/gen/tag_data_pb';
-import { AsyncQueue } from '@esfx/async-queue';
+import Redis, { RedisOptions } from "ioredis";
+import { TagData, TagDataObjectIdentifier, TagDataSnapshot } from "../../../submodules/src/gen/tag_data_pb";
+
+type Callback = (err: Error | null, snapshot?: TagDataSnapshot, delta?: TagData) => void;
 
 class Subscriber {
     private redisClient: Redis;
-    private asyncQueue: AsyncQueue<TagDataEnvelope | TagDataSnapshot>;
-    private redisStreamName: string;
-    private lastReadId: string;
+    private tagDataObjIdentifier: TagDataObjectIdentifier;
 
-    constructor(redisOptions: RedisOptions, redisStreamName: string, lastReadId?: string) {
+    constructor(
+        redisOptions: RedisOptions,
+        tagDataObjIdentifier: TagDataObjectIdentifier
+    ) {
         this.redisClient = new Redis(redisOptions);
-        this.asyncQueue = new AsyncQueue<TagDataEnvelope | TagDataSnapshot>();
-        this.redisStreamName = redisStreamName;
-        this.lastReadId = lastReadId || '0';
+        this.tagDataObjIdentifier = tagDataObjIdentifier;
 
-        this.redisClient.on('connect', async () => {
-            console.log('Connected to Redis server');
+        this.redisClient.on("connect", async () => {
+            console.log("Connected to Redis server");
         });
 
-        this.redisClient.on('error', async (error) => {
+        this.redisClient.on("error", async (error) => {
             console.error(`Redis error: ${error}`);
         });
     }
 
-    async startSubscription(): Promise<void> {
-        // Read initial snapshot from Redis hash
-        const initialSnapshot: Record<string, string> = await this.redisClient.hgetall(this.redisStreamName);
-        const tagDataSnapshot = new TagDataSnapshot();
-        Object.keys(initialSnapshot).forEach((key) => {
-            const tagEnvelope = TagDataEnvelope.fromJsonString(initialSnapshot[key]);
-            tagDataSnapshot.snapshot[key] = tagEnvelope;
-        });
-        tagDataSnapshot.sequenceNumber = initialSnapshot['seqno'] || '';
-        this.asyncQueue.put(tagDataSnapshot); // Enqueue as TagDataSnapshot
+    public async streamSnapshotAndDeltas(callback: Callback): Promise<void> {
+        try {
+            // Fetch the snapshot from Redis
+            const snapshotSerialized = await this.redisClient.hgetall(this.tagDataObjIdentifier.name);
 
-        while (true) {
-            const response = await this.redisClient.xread(
-                'COUNT',
-                10, // read maximum 10 messages per call
-                'BLOCK',
-                5000, // blocking TIMEOUT in milliseconds
-                'STREAMS',
-                this.redisStreamName,
-                this.lastReadId
-            );
-            if (response) {
-                for (const message of response[0][1]) {
-                    const [id, fields] = message;
-                    console.log(`Received message ID: ${id}`);
-                    console.log('Fields:', fields);
+            // Deserialize the snapshot using TagDataSnapshot.fromBinary method
+            const snapshot = TagDataSnapshot.fromBinary(Buffer.from(snapshotSerialized));
 
-                    const tagData = TagData.fromJsonString(fields[3]);
-                    const tagDataEnvelope = new TagDataEnvelope({ tagData, sequenceNumber: id });
+            // Call the callback with the fetched snapshot
+            callback(null, snapshot);
 
-                    this.asyncQueue.put(tagDataEnvelope);
-                    this.lastReadId = id; // save the ID for the next xread call
+            // Start streaming deltas in a loop using XREAD with BLOCK option
+            const readDeltas = async () => {
+                const streamKey = `delta:${this.tagDataObjIdentifier.name}`;
+                const deltas = await this.redisClient.xread("BLOCK", 0, "STREAMS", streamKey, "$");
 
-                    // Store the sequence number in the Redis hash
-                    const redisSnapshotKey: string = this.redisStreamName;
-                    await this.redisClient.hset(redisSnapshotKey, 'seqno', id);
-                }
-            }
+                // Deserialize the delta using TagData.fromBinary method
+                const delta = TagData.fromBinary(Buffer.from(deltas));
+
+                // Call the callback with the fetched delta
+                callback(null, undefined, delta);
+
+                // Keep reading deltas
+                readDeltas();
+            };
+
+            // Start reading deltas
+            readDeltas();
+
+        } catch (error) {
+            // Call the callback with the error
+            callback(error);
         }
     }
 }
