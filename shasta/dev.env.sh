b8283e13a405806bcda9606060c021ffe7ddc12b
John Davis <john.davis@airtime.com>
2023-06-20 14:06:02 -0700

    test, env vars

--- shasta/dev.env.sh
+++ shasta/dev.env.sh
@@ -3,3 +3,7 @@ nvm use 16.17.0
 export GIT_SSH_COMMAND=ssh
 export CC=clang
 export CXX='clang++ -stdlib=libc++'
+
+export KAFKA_BROKERS="b-1.shastamskautomation78.iugl10.c21.kafka.us-east-1.amazonaws.com:9098,b-3.shastamskautomation78.iugl10.c21.kafka.us-east-1.amazonaws.com:9098,b-2.shastamskautomation78.iugl10.c21.kafka.us-east-1.amazonaws.com:9098"
+export REDIS_HOST="clustercfg.shasta-redis-automation788.3ezarj.memorydb.us-east-1.amazonaws.com"
+export REDIS_PORT="6379"
