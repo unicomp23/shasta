2b5a56ea9cf2aeb32b0293386a4a574d6227cf2d
John Davis <john.davis@airtime.com>
2023-06-26 16:06:10 -0700

    test refactor

--- shasta/src/lib/pubsub/roundtrip.test.ts
+++ shasta/src/lib/pubsub/roundtrip.test.ts
@@ -1,143 +1,133 @@
-import {ITopicConfig} from "kafkajs";
-import Redis, {Cluster, ClusterNode, RedisOptions} from 'ioredis';
-import {TagData, TagDataObjectIdentifier} from "../../../submodules/src/gen/tag_data_pb";
-import {Publisher} from './publisher';
-import {Subscriber} from './subscriber';
-import {Worker} from './worker';
-import {createKafka} from "../kafka/createKafka";
+import { ITopicConfig } from "kafkajs";
+import Redis, { Cluster, ClusterNode, RedisOptions } from "ioredis";
+import { TagData, TagDataObjectIdentifier } from "../../../submodules/src/gen/tag_data_pb";
+import { Publisher } from "./publisher";
+import { Subscriber } from "./subscriber";
+import { Worker } from "./worker";
+import { createKafka } from "../kafka/createKafka";
 import crypto from "crypto";
-import {Deferred, delay} from "@esfx/async";
-import {env} from "process";
+import { Deferred, delay } from "@esfx/async";
+import { env } from "process";
 
 const REDIS_OPTIONS: RedisOptions = {
     host: process.env.REDIS_HOST || "localhost",
     port: parseInt(process.env.REDIS_PORT || "6379"),
-    tls:{},
+    tls: {},
 };
 
 // Kafka topic for testing
 const kafkaTopic = `test_topic-${crypto.randomUUID()}`;
 
-describe('End-to-End Test 2', () => {
+async function waitFor(durationInMs: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, durationInMs));
+}
+
+async function setup() {
+    const identifier = new TagDataObjectIdentifier();
+    identifier.appId = `some-app-id-${crypto.randomUUID()}`;
+    identifier.tag = `tag-id-${crypto.randomUUID()}`;
+    identifier.scope = `scope-id-${crypto.randomUUID()}`;
+    identifier.name = `name-${crypto.randomUUID()}`;
+
+    const kafka = createKafka(`test-kafka-id-${crypto.randomUUID()}`);
+    const admin = kafka.admin();
+    await admin.connect();
+    const topicConfig: ITopicConfig = {
+        topic: kafkaTopic,
+    };
+    await admin.createTopics({
+        topics: [topicConfig],
+    });
+    await admin.disconnect();
+
+    const publisher = new Publisher(kafka, kafkaTopic);
+    await publisher.connect();
+
+    const redisClient = new Cluster([{ host: env.REDIS_HOST, port: parseInt(env.REDIS_PORT || "6379") }], { dnsLookup: (address, callback) => callback(null, address), redisOptions: { tls: {}, }, });
+
+    const subscriber = new Subscriber(identifier);
+
+    const groupId = `test-group-id-${crypto.randomUUID()}`;
+    const worker = new Worker(kafka, groupId, kafkaTopic);
+    await worker.groupJoined();
+
+    return { publisher, subscriber, worker, redisClient, identifier };
+}
+
+async function teardown(publisher: Publisher, subscriber: Subscriber, worker: Worker, redisClient: Cluster) {
+    await worker.shutdown();
+    await publisher.disconnect();
+    await redisClient.quit();
+    await subscriber.disconnect();
+}
+
+describe("End-to-End Test 2", () => {
     let publisher: Publisher;
     let subscriber: Subscriber;
     let worker: Worker;
     let redisClient: Cluster;
-    const identifier = new TagDataObjectIdentifier();
-
+    let identifier: TagDataObjectIdentifier;
 
     beforeAll(async () => {
-        identifier.appId = `some-app-id-${crypto.randomUUID()}`;
-        identifier.tag = `tag-id-${crypto.randomUUID()}`;
-        identifier.scope = `scope-id-${crypto.randomUUID()}`
-        identifier.name = `name-${crypto.randomUUID()}`;
-
-        // Create the Kafka instance
-        const kafka = createKafka(`test-kafka-id-${crypto.randomUUID()}`);
-
-        // Create the Kafka admin interface
-        const admin = kafka.admin();
-        await admin.connect();
-
-        // Pre-create the Kafka topic
-        const topicConfig: ITopicConfig = {
-            topic: kafkaTopic,
-        };
-        await admin.createTopics({
-            topics: [topicConfig],
-        });
-        console.log({topicConfig})
-
-        const listTopics = await admin.listTopics();
-        console.log({listTopics})
-        if(!listTopics.includes(kafkaTopic))
-            console.error(`topic not confirmed: `, {kafkaTopic});
-        else
-            console.log(`topic confirmed: `, {kafkaTopic});
-
-
-        // Disconnect the admin interface
-        await admin.disconnect();
-
-        // Create and connect the Publisher
-        publisher = new Publisher(kafka, kafkaTopic);
-        await publisher.connect();
-
-        // Create the Redis client
-        redisClient = new Cluster ([ { host: env.REDIS_HOST, port: parseInt(env.REDIS_PORT || "6379") }], { dnsLookup: (address, callback) => callback (null, address), redisOptions: { tls: {}, }, });
-        console.log('redis: ', env.REDIS_HOST);
-
-        // Create the Subscriber
-        subscriber = new Subscriber(identifier);
-
-        // Create the Worker
-        const groupId = `test-group-id-${crypto.randomUUID()}`;
-        worker = new Worker(kafka, groupId, kafkaTopic);
-        await worker.groupJoined();
+        const resources = await setup();
+        publisher = resources.publisher;
+        subscriber = resources.subscriber;
+        worker = resources.worker;
+        redisClient = resources.redisClient;
+        identifier = resources.identifier;
     });
 
     afterAll(async () => {
-        // Disconnect and cleanup resources
-        await worker.shutdown();
-        await publisher.disconnect();
-        await redisClient.quit();
-        await subscriber.disconnect();
+        await teardown(publisher, subscriber, worker, redisClient);
     });
 
-    it('should process messages from Publisher to Worker via Redis Subscriber', async () => {
+    it("should process messages from Publisher to Worker via Redis Subscriber", async () => {
         const tagData = new TagData();
         tagData.identifier = identifier.clone();
-        tagData.data = 'Test Value';
+        tagData.data = "Test Value";
 
         // Send TagData message from Publisher
         await publisher.send(tagData);
-        console.log('publisher.send', {tagData})
 
         // Wait for the message to reach Redis Subscriber through Worker
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await waitFor(2000);
 
-        // Use additional assertions and expectations to validate the processing and persistence of the message
-        // For example, check if the message is present in the Redis snapshot using Redis client API, or assert specific actions performed by the Worker or Subscriber
-        // You can also modify the Worker or Subscriber classes to expose additional methods or properties to aid in testing and assertions
-        // Use Redis client to retrieve data from Redis and assert on the expected state
+        const redisSnapshotKey = getRedisSnapshotKey(identifier);
+        const redisSnapshotData = await redisClient.hgetall(redisSnapshotKey);
 
-        // Example assertion: Check if the message has been added to Redis snapshot
-        const snapIdentifier = identifier.clone();
-        snapIdentifier.name = "";
-        const redisSnapshotKey = Buffer.from(snapIdentifier.toBinary()).toString("base64");
-        const commonRedisSnapshotKey = `{${redisSnapshotKey}}:snap:`;
-
-        const redisSnapshotData = await redisClient.hgetall(commonRedisSnapshotKey);
-        console.log('redisClient.hgetall.2', {commonRedisSnapshotKey, redisSnapshotData});
-
-// Retrieve snapshots from Subscriber
+        // Retrieve snapshots from Subscriber
         const snapshotsQueue = await subscriber.stream();
 
-// Dequeue snapshots until queue is empty
+        // Dequeue snapshots until the queue is empty
         const message = await snapshotsQueue.get();
 
-// Check the specifics of the message, ensuring that the properties are present
+        // Check the specifics of the message, ensuring that the properties are present
         const snapshot = message.snapshot;
-        console.log('redisClient.hgetall.snap', {snapshot});
+        expect(snapshot).toBeDefined();
 
         // Publish another TagData delta
         const tagDataDelta = new TagData();
         tagDataDelta.identifier = identifier.clone();
-        tagDataDelta.data = 'Test Delta Value';
+        tagDataDelta.data = "Test Delta Value";
         await publisher.send(tagDataDelta);
-        console.log('publisher.send', {tagData: tagDataDelta})
 
         // Wait for the delta message to reach Redis Subscriber through Worker
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await waitFor(2000);
 
-        // Dequeue deltas until queue is empty
+        // Dequeue deltas until the queue is empty
         const deltaMessage = await snapshotsQueue.get();
 
         // Check the specifics of the delta message
         const delta = deltaMessage.delta;
-        console.log('delta message received', {delta});
+        expect(delta).toBeDefined();
 
         // Assert the field in the delta
         expect(delta?.data).toEqual(tagDataDelta.data);
     });
+
+    function getRedisSnapshotKey(identifier: TagDataObjectIdentifier): string {
+        const snapIdentifier = identifier.clone();
+        snapIdentifier.name = "";
+        return Buffer.from(snapIdentifier.toBinary()).toString("base64");
+    }
 });
