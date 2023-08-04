1cb7323d25e740ebfcf75e71a8523ed41eba1304
John Davis <john.davis@airtime.com>
2023-08-04 16:23:09 -0700

    fix

--- shasta/src/lib/pubsub/loadtest.spec.ts
+++ shasta/src/lib/pubsub/loadtest.spec.ts
@@ -137,12 +137,14 @@ async function setupKafkaPairs(pairs: TestRef[], n: number): Promise<void> {
         setupPromise.then((ref) => { pendingSetup.put(ref); pairs.push(ref); });
         if(pendingSetup.size >= maxConcurrentConnects) {
             const ref = await pendingSetup.get();
+            pairs.push(ref);
             slog.info("setup complete", { i, groupId, kafkaTopicLoad });
         }
     }
 
     while(pendingSetup.size > 0) {
         const ref = await pendingSetup.get();
+        pairs.push(ref);
         slog.info("setup complete", { i: n, groupId, kafkaTopicLoad });
     }
 }
