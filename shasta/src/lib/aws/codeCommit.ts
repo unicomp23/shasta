14c06f49554b65e5f6060396bfeae4fe51d4e10d
John Davis <john.davis@airtime.com>
2023-09-25 14:27:36 -0700

    block on sqs queue

--- shasta/src/lib/aws/codeCommit.ts
+++ shasta/src/lib/aws/codeCommit.ts
@@ -4,36 +4,38 @@ const sqs = new SQS({ region: 'us-east-1' }); // replace with your region todo
 
 async function receiveMessages() {
   const params = {
-    QueueUrl: process.env.SQS_QUEUE_URL || 'default_queue_url', // replace 'default_queue_url' with a valid default value
+    QueueUrl: process.env.SQS_QUEUE_URL || 'default_queue_url',
     MaxNumberOfMessages: 10,
     VisibilityTimeout: 60,
-    WaitTimeSeconds: 0
+    WaitTimeSeconds: 20 // Set to 20 seconds
   };
-  
-  try {
-    const data = await sqs.receiveMessage(params).promise();
 
-    if (data.Messages) {
-      for (const message of data.Messages) {
-        console.log('Message:', message.Body);
+  while (true) {
+    try {
+      const data = await sqs.receiveMessage(params).promise();
 
-        const deleteParams = {
-          QueueUrl: params.QueueUrl,
-          ReceiptHandle: message.ReceiptHandle!
-        };
+      if (data.Messages) {
+        for (const message of data.Messages) {
+          console.log('Message:', message.Body);
 
-        try {
-          await sqs.deleteMessage(deleteParams).promise();
-          console.log('Message deleted', message.MessageId);
-        } catch (deleteError) {
-          console.log('Delete error', deleteError);
+          const deleteParams = {
+            QueueUrl: params.QueueUrl,
+            ReceiptHandle: message.ReceiptHandle!
+          };
+
+          try {
+            await sqs.deleteMessage(deleteParams).promise();
+            console.log('Message deleted', message.MessageId);
+          } catch (deleteError) {
+            console.log('Delete error', deleteError);
+          }
         }
+      } else {
+        console.log('No messages to process');
       }
-    } else {
-      console.log('No messages to process');
+    } catch (receiveError) {
+      console.log('Receive error', receiveError);
     }
-  } catch (receiveError) {
-    console.log('Receive error', receiveError);
   }
 }
 
