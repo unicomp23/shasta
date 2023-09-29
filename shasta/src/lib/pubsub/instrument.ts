55b74c9b19adbd953872a9c07956bd1954eeaa10
John Davis <john.davis@airtime.com>
2023-09-29 13:39:49 -0700

    intrumentation merge

--- shasta/src/lib/pubsub/instrument.ts
+++ shasta/src/lib/pubsub/instrument.ts
@@ -5,8 +5,18 @@ import {numCPUs} from "./loadtest.spec";
 import * as os from 'os';
 import * as Redis from 'ioredis';
 import assert from 'assert';
+import fs from 'fs';
 
-export class Timestamps {
+interface ITimestamps {
+    beforePublish: number;
+    afterPublish: number;
+    afterConsume: number;
+    afterWorkerXAdd: number;
+    afterWorkerHSet: number;
+    afterSubscribeXRead: number;
+}
+
+export class Timestamps implements ITimestamps {
     public beforePublish: number = 0;
     public afterPublish: number = 0;
     public afterConsume: number = 0;
@@ -16,7 +26,7 @@ export class Timestamps {
 }
 
 export class Instrumentation {
-    private readonly timestamps = new Map<string, Timestamps>();
+    private readonly timestamps = new Map<string, ITimestamps>();
 
     private static _instance: Instrumentation;
 
@@ -37,7 +47,7 @@ export class Instrumentation {
         this._enabled = value;
     }
 
-    public getTimestamps(tdoid: TagDataObjectIdentifier): Timestamps {
+    public getTimestamps(tdoid: TagDataObjectIdentifier): ITimestamps {
         const clone = tdoid.clone();
         const key = Buffer.from(clone.toBinary()).toString("base64");
         if (!this.timestamps.has(key)) {
@@ -48,7 +58,7 @@ export class Instrumentation {
 
     public dump() {
         const timestampsObj = Array.from(this.timestamps).reduce((obj: {
-            [key: string]: Timestamps
+            [key: string]: ITimestamps
         }, [key, value]) => {
             obj[key] = value;
             return obj;
@@ -64,7 +74,7 @@ export class Instrumentation {
         );
     }
 
-    public async dumpToRedisStream() {
+    public async writeNodeInstrumentData() {
         assert(process.env.REDIS_HOST, 'REDIS_HOST is not defined in the environment variables');
         assert(process.env.REDIS_PORT, 'REDIS_PORT is not defined in the environment variables');
 
@@ -84,7 +94,7 @@ export class Instrumentation {
             });
 
             const timestampsObj = Array.from(this.timestamps).reduce((obj: {
-                [key: string]: Timestamps
+                [key: string]: ITimestamps
             }, [key, value]) => {
                 obj[key] = value;
                 return obj;
@@ -140,16 +150,36 @@ export class Instrumentation {
                 }
                 for (const item of result[0][1]) {
                     const id = item[0];
-                    const data = JSON.parse(item[1][1]);
+                    const [field, val] = item[1];
+                    const data = JSON.parse(val);
                     const hostname = data.hostname;
                     if (!instrumentDataByHost[hostname]) {
                         instrumentDataByHost[hostname] = [];
                     }
-                    instrumentDataByHost[hostname].push(data);
+                    // Cast data to ITimestamps
+                    const timestamps: ITimestamps = data.timestamps;
+                    instrumentDataByHost[hostname].push(timestamps);
                     lastId = id;
                 }
             }
             console.log(`Instrument data by host: ${JSON.stringify(instrumentDataByHost)}`);
+            const schema = {
+                type: "object",
+                properties: {
+                    beforePublish: {type: "number"},
+                    afterPublish: {type: "number"},
+                    afterConsume: {type: "number"},
+                    afterWorkerXAdd: {type: "number"},
+                    afterWorkerHSet: {type: "number"}
+                }
+            };
+            fs.writeFileSync('/tmp/instrumentation.json', JSON.stringify(instrumentDataByHost), {flag: 'w'});
+            fs.writeFileSync('/tmp/instrumentation_schema.json', JSON.stringify(schema), {flag: 'w'});
+
+            // Use the following SCP command to download files from the EC2 instance to your MacBook
+            // Replace <username>, <ec2-ip-address>, <source-path> and <destination-path> with actual values
+            // This is just a comment, not executable code. To execute, you need to run this command in your terminal
+            // 'scp -i /path/to/your/key.pem <username>@<ec2-ip-address>:<source-path> <destination-path>'
         } catch (error) {
             console.error('Error during xread operation:', error);
         } finally {
@@ -160,5 +190,7 @@ export class Instrumentation {
                 console.error('Error during quit operation:', error);
             }
         }
+
     }
 }
+
