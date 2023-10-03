acf7b440ba2b6a31671a322a78cef82a44673c3e
John Davis <john.davis@airtime.com>
2023-10-03 13:53:57 -0700

    009

--- shasta/src/lib/pubsub/loadtest.spec.ts
+++ shasta/src/lib/pubsub/loadtest.spec.ts
@@ -28,7 +28,7 @@ describe("End-to-End Load Test", () => {
         const exitQueue = new AsyncQueue<number>();
 
         if (cluster.default.isPrimary) {
-            const randomTag = "008"; // todo crypto.randomUUID();
+            const randomTag = "009"; // todo crypto.randomUUID();
             const kafkaTopicLoad = `test_topic_load-${randomTag}`;
             const groupId = `test_group_id-${randomTag}`;
 
