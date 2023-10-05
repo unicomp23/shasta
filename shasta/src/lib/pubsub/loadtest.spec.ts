36cde5f94c7250bf900a8c405cef31138d3431c4
John Davis <john.davis@airtime.com>
2023-10-04 17:03:45 -0700

    remove cpu clustering

--- shasta/src/lib/pubsub/loadtest.spec.ts
+++ shasta/src/lib/pubsub/loadtest.spec.ts
@@ -7,7 +7,7 @@ import * as cluster from 'cluster';
 import {RedisKeyCleanup} from "./redisKeyCleanup";
 import {env} from "process";
 
-export const numCPUs = 1; //os.cpus().length / 2;
+export const numCPUs = 1;
 
 describe("End-to-End Load Test", () => {
     it("should load test messages from Publisher->Worker->Redis Subscriber", async () => {
@@ -25,52 +25,12 @@ describe("End-to-End Load Test", () => {
             .catch(console.error); ***/
 
         console.log(`numCPUs: ${numCPUs}`);
-        const exitQueue = new AsyncQueue<number>();
+        const kafkaTopicLoad = process.env.KAFKA_TOPIC_LOAD;
+        if (kafkaTopicLoad === undefined) throw new Error("KAFKA_TOPIC_LOAD environment variable is not set");
+        const groupId = process.env.KAFKA_GROUP_ID;
+        if (groupId === undefined) throw new Error("KAFKA_GROUP_ID environment variable is not set");
+        const sanityCountSub = await loadTest(kafkaTopicLoad, numCPUs, groupId);
+        expect(sanityCountSub).to.equal(pairCount * messageCount);
 
-        if (cluster.default.isPrimary) {
-            const randomTag = "019"; // todo crypto.randomUUID();
-            const kafkaTopicLoad = `test_topic_load-${randomTag}`;
-            const groupId = `test_group_id-${randomTag}`;
-
-            // Fork workers.
-            for (let i = 0; i < numCPUs; i++) {
-                const worker = cluster.default.fork({
-                    KAFKA_TOPIC_LOAD: kafkaTopicLoad,
-                    KAFKA_GROUP_ID: groupId
-                });
-                console.log(`Worker ${worker.process.pid} forked, primary`);
-            }
-
-            cluster.default.on('exit', (worker, code, signal) => {
-                console.log(`Worker ${worker.process.pid} died, worker`);
-                if (worker.process.pid !== undefined)
-                    exitQueue.put(worker.process.pid);
-            });
-
-            cluster.default.on('message', (worker, message, handle) => {
-                console.log(`Worker ${worker.process.pid} sent message: ${message}`);
-            });
-
-            for (let i = 0; i < numCPUs; i++) {
-                const pid = await exitQueue.get();
-                console.log(`Worker ${pid} died, primary`);
-            }
-        } else {
-            const kafkaTopicLoad = process.env.KAFKA_TOPIC_LOAD;
-            if (kafkaTopicLoad === undefined) throw new Error("KAFKA_TOPIC_LOAD environment variable is not set");
-            const groupId = process.env.KAFKA_GROUP_ID;
-            if (groupId === undefined) throw new Error("KAFKA_GROUP_ID environment variable is not set");
-            const sanityCountSub = await loadTest(kafkaTopicLoad, numCPUs, groupId);
-            expect(sanityCountSub).to.equal(pairCount * messageCount);
-            if (process.send !== undefined) {
-                process.send(JSON.stringify({
-                    sanityCountSub,
-                    pid: process.pid
-                }));
-                process.exit(0);
-            }
-        }
-
-        process.exit(0); // todo remove
     });
 });
