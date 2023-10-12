36178715bbb5b660e4cbd12abe81888eadbf98f0
John Davis <john.davis@cantina.ai>
2023-10-12 20:48:51 +0000

    040, 32 nodes, 32 producers per

--- shasta/src/lib/pubsub/loadtest.ts
+++ shasta/src/lib/pubsub/loadtest.ts
@@ -13,7 +13,7 @@ import {envVarsSync} from "../../automation";
 import {env} from "process";
 import { RedisKeyCleanup } from './redisKeyCleanup';
 
-export const pairCount = 128; // Number of publisher/subscriber pairs
+export const pairCount = 32; // Number of publisher/subscriber pairs
 export const messageCount = 64; // Number of published messages per pair
 
 let sanityCountSub = 0;
@@ -275,7 +275,7 @@ export async function main() {
         .catch(console.error); ***/
 
     console.log(`numCPUs: ${numCPUs}`);
-    const randomTag = "039"; // todo crypto.randomUUID();
+    const randomTag = "040"; // todo crypto.randomUUID();
     const kafkaTopicLoad = `test_topic_load-${randomTag}`;
     const groupId = `test_group_id-${randomTag}`;
 
