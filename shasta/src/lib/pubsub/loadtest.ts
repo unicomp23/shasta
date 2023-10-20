4f0c30b3552b375abc753d07cd20fe34f09ee8fc
John Davis <john.davis@cantina.ai>
2023-10-20 23:13:39 +0000

    061, wip serverless

--- shasta/src/lib/pubsub/loadtest.ts
+++ shasta/src/lib/pubsub/loadtest.ts
@@ -286,7 +286,7 @@ export async function main() {
         .catch(console.error); ***/
 
     console.log(`numCPUs: ${numCPUs}`);
-    const randomTag = "060"; // todo crypto.randomUUID();
+    const randomTag = "061"; // todo crypto.randomUUID();
     const kafkaTopicLoad = `test_topic_load-${randomTag}`;
     const groupId = `test_group_id-${randomTag}`;
 
