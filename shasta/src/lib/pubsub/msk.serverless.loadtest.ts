9dd9a71ea44a852069b86f3d2bf3bff7e3c08abc
EC2 Default User <ec2-user@ip-10-0-1-139.ec2.internal>
2023-10-20 21:56:48 +0000

    wip, serverless

---
+++ shasta/src/lib/pubsub/msk.serverless.loadtest.ts
@@ -0,0 +1,16 @@
+import * as AWS from 'aws-sdk';
+AWS.config.update({region:'us-east-1'}); // replace 'us-east-1' with your region
+const kafka = new AWS.Kafka();
+
+if (!process.env.MSK_SERVERLESS_CLUSTER_ARN) {
+  throw new Error('MSK_SERVERLESS_CLUSTER_ARN is not defined');
+}
+
+const params = {
+  ClusterArn: process.env.MSK_SERVERLESS_CLUSTER_ARN // ARN of your MSK cluster
+};
+
+kafka.getBootstrapBrokers(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
