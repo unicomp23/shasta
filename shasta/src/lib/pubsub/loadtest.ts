28e1d795d5456a82216eaf2f2a178dd1731a019c
John Davis <john.davis@cantina.ai>
2023-10-27 15:27:39 +0000

    092

--- shasta/src/lib/pubsub/loadtest.ts
+++ shasta/src/lib/pubsub/loadtest.ts
@@ -293,7 +293,7 @@ export async function main() {
         .catch(console.error); ***/
 
     console.log(`numCPUs: ${numCPUs}`);
-    const randomTag = "091"; // todo crypto.randomUUID();
+    const randomTag = "092"; // todo crypto.randomUUID();
     const kafkaTopicLoad = `test_topic_load-${randomTag}`;
     const groupId = `test_group_id-${randomTag}`;
 
