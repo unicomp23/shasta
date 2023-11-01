e221050a24fb863456ac4ae8db8b672f36e1e9fe
John Davis <john.davis@cantina.ai>
2023-11-01 21:57:33 +0000

    aws sdk snippet

--- shasta/src/lib/kafka/createKafka.ts
+++ shasta/src/lib/kafka/createKafka.ts
@@ -26,3 +26,37 @@ export function createKafka(clientId: string, region: string = 'us-east-1', numC
         });
     }
 }
+
+/***
+
+const { Kafka } = require('kafkajs');
+const AWS = require('aws-sdk');
+
+// Create a new AWS STS client
+const sts = new AWS.STS();
+
+async function createKafkaClient() {
+  // Get the IAM role credentials
+  const credentials = await sts.getCredentialsForRole({ RoleArn: 'your-iam-role-arn' }).promise();
+
+  // Create a new Kafka client
+  const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['your-msk-broker1', 'your-msk-broker2'], // replace with your MSK brokers
+    ssl: true, // Enable TLS
+    sasl: {
+      mechanism: 'aws',
+      awsAccessKeyId: credentials.AccessKeyId,
+      awsSecretAccessKey: credentials.SecretAccessKey,
+      awsSessionToken: credentials.SessionToken,
+    },
+  });
+
+  return kafka;
+}
+
+createKafkaClient().then(kafka => {
+  // You can now use the Kafka client to produce and consume messages
+}).catch(console.error);
+
+ */
