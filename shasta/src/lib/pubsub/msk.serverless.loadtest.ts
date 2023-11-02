37cca4b9ecb9147ebde9ad7860f9439d704409f9
John Davis <john.davis@cantina.ai>
2023-11-02 18:04:16 +0000

    troubleshooting

--- shasta/src/lib/pubsub/msk.serverless.loadtest.ts
+++ shasta/src/lib/pubsub/msk.serverless.loadtest.ts
@@ -2,23 +2,23 @@ import * as AWS from 'aws-sdk';
 import {env} from "process";
 
 
-AWS.config.update({region:'us-east-1'}); // replace 'us-east-1' with your region
-const kafka = new AWS.Kafka();
-
-if (!process.env.MSK_SERVERLESS_CLUSTER_ARN) {
-  throw new Error('MSK_SERVERLESS_CLUSTER_ARN is not defined');
-}
-
-const params = {
-  ClusterArn: process.env.MSK_SERVERLESS_CLUSTER_ARN // ARN of your MSK cluster
-};
-
-kafka.getBootstrapBrokers(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-
-export function getServerlessBootstrapBrokers() {
+function getServerlessBootstrapBrokers() {
+  AWS.config.update({region:'us-east-1'}); // replace 'us-east-1' with your region
+  const kafka = new AWS.Kafka();
+  
+  if (!process.env.MSK_SERVERLESS_CLUSTER_ARN) {
+    throw new Error('MSK_SERVERLESS_CLUSTER_ARN is not defined');
+  }
+  
+  const params = {
+    ClusterArn: process.env.MSK_SERVERLESS_CLUSTER_ARN // ARN of your MSK cluster
+  };
+  
+  kafka.getBootstrapBrokers(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+  
   return new Promise<string>((resolve, reject) => {
     kafka.getBootstrapBrokers(params, function(err, data) {
       if (err) {
