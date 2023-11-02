fe5fb3846ba0a1bc19e02f950101c2d705f59c75
John Davis <john.davis@cantina.ai>
2023-11-02 17:55:55 +0000

    logging

--- shasta/src/lib/kafka/createKafka.ts
+++ shasta/src/lib/kafka/createKafka.ts
@@ -2,11 +2,13 @@ import {Kafka, logLevel} from "kafkajs";
 import {kafkaLogLevel} from "./constants";
 import {createMechanism} from "@jm18457/kafkajs-msk-iam-authentication-mechanism";
 import {env} from "process";
+import { slog } from "../logger/slog";
 
 export const singleServerTcpSpacingMillis = 100;
 
 export function createKafka(clientId: string, region: string = 'us-east-1', numCPUs = 1): Kafka {
     const bootstrapEndpoints = env.KAFKA_BROKERS?.split(",") || [];
+    slog.info("createKafka", bootstrapEndpoints);
     if(process.env.USING_IAM === "true") {
         console.log('kafka w/ iam')
         return new Kafka({
