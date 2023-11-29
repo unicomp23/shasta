452f53d020186e73f157851268672d8e0777ca9f
John Davis <john.davis@cantina.ai>
2023-11-29 00:35:24 +0000

    143

--- shasta/src/lib/pubsub/loadtest.ts
+++ shasta/src/lib/pubsub/loadtest.ts
@@ -12,8 +12,8 @@ import {Instrumentation} from "./instrument";
 import {env} from "process";
 import {createAndVerifyKafkaTopic, generateTopicAndGroupId} from "./topic";
 
-export const pairCount = 32; // todo 8; // Number of publisher/subscriber pairs
-export const messageCount = 256; // todo restore 64 // Number of published messages per pair
+export const pairCount = 8; // Number of publisher/subscriber pairs
+export const messageCount = 64 // Number of published messages per pair
 
 let sanityCountSub = 0;
 let sanityCountPub = 0;
