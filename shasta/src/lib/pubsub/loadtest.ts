920983abb1ecc1efe154779ef60f0bdfebb2a877
John Davis <john.davis@cantina.ai>
2024-02-23 03:00:59 +0000

    261, 6 hr test, no redis

--- shasta/src/lib/pubsub/loadtest.ts
+++ shasta/src/lib/pubsub/loadtest.ts
@@ -14,13 +14,13 @@ import {createAndVerifyKafkaTopic, generateTopicAndGroupId} from "./topic";
 import fs from "fs";
 
 export const pairCount = 8; // Number of publisher/subscriber pairs
-export const messageCount = 14400; //1800; // Number of published messages per pair
+export const messageCount = 43200; //1800; // Number of published messages per pair
 
 let sanityCountSub = 0;
 let sanityCountPub = 0;
 
 const workerModulo = 1;
-const eventSpacingMillis = 20; //1000;
+const eventSpacingMillis = 500; //1000;
 
 const pairs = new Array<TestRef>();
 
