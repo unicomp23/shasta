3a7e4e3b1eea57be28caba289ced772efabec74c
John Davis <john.davis@airtime.com>
2024-03-12 15:30:07 -0700

    fix

--- tools/send-commands.sh
+++ tools/send-commands.sh
@@ -14,11 +14,13 @@ for instance in "${instance_array[@]}"
 do
   # Log the instance ID
   echo "Sending command to instance: $instance"
+  # Define the command to send
+  cmd_to_send="sudo -u ec2-user -i /bin/bash -c 'mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=\"--max-old-space-size=16384 --expose-gc\" npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1'"
   # Send the command to the instance
   aws ssm send-command \
       --region us-east-1 \
       --document-name "AWS-RunShellScript" \
-      --parameters '{"commands":["sudo -u ec2-user -i /bin/bash -c \"mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=\'--max-old-space-size=16384 --expose-gc\' npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1\""], "executionTimeout":["86400"]}' \
+      --parameters "{\"commands\":[\"$cmd_to_send\"], \"executionTimeout\":[\"86400\"]}" \
       --instance-ids "$instance" \
       --timeout-seconds 86400 >/dev/null 2>&1
   # Sleep for 50ms before moving on to the next instance
@@ -39,11 +41,13 @@ for instance in "${instance_array[@]}"
 do
   # Log the instance ID and command number
   echo "Sending command to instance: $instance"
+  # Define the command to send
+  cmd_to_send="sudo -u ec2-user -i /bin/bash -c 'mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=\"--max-old-space-size=16384 --expose-gc\" npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1'"
   # Send the command to the instance
   aws ssm send-command \
       --region us-east-1 \
       --document-name "AWS-RunShellScript" \
-      --parameters '{"commands":["sudo -u ec2-user -i /bin/bash -c \"mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=\'--max-old-space-size=16384 --expose-gc\' npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1\""], "executionTimeout":["86400"]}' \
+      --parameters "{\"commands\":[\"$cmd_to_send\"], \"executionTimeout\":[\"86400\"]}" \
       --instance-ids "$instance" \
       --timeout-seconds 86400 >/dev/null 2>&1
   # Sleep for 50ms before sending the next command
