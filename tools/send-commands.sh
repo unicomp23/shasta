4a9122209471dceee38486415850eaeb762e4719
John Davis <john.davis@airtime.com>
2024-03-12 16:06:00 -0700

    gc w/ large heap, works

--- tools/send-commands.sh
+++ tools/send-commands.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define NODE_OPTIONS outside the loops
+export NODE_OPTIONS="--max-old-space-size=16384 --expose-gc"
+
 # Get the list of instance IDs for the workers
 instances=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Role,Values=worker" --query "Reservations[*].Instances[*].InstanceId" --output text --region us-east-1)
 
@@ -9,19 +12,16 @@ instance_array=($instances)
 # Print out the number of instances
 echo "Number of workers: ${#instance_array[@]}"
 
-# Loop over the instances
+# Loop over the instances for workers
 for instance in "${instance_array[@]}"
 do
-  # Log the instance ID
   echo "Sending command to instance: $instance"
-  # Send the command to the instance
   aws ssm send-command \
       --region us-east-1 \
       --document-name "AWS-RunShellScript" \
-      --parameters '{"commands":["sudo -u ec2-user -i /bin/bash -c \"mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=--max-old-space-size=16384 npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1\""], "executionTimeout":["86400"]}' \
+      --parameters "{\"commands\":[\"sudo -u ec2-user -i /bin/bash -c 'mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1'\"]}" \
       --instance-ids "$instance" \
       --timeout-seconds 86400 >/dev/null 2>&1
-  # Sleep for 50ms before moving on to the next instance
   sleep 0.05
 done
 
@@ -34,18 +34,15 @@ instance_array=($instances)
 # Print out the number of instances
 echo "Number of consumers: ${#instance_array[@]}"
 
-# Loop over the instances
+# Loop over the instances for consumers
 for instance in "${instance_array[@]}"
 do
-  # Log the instance ID and command number
   echo "Sending command to instance: $instance"
-  # Send the command to the instance
   aws ssm send-command \
       --region us-east-1 \
       --document-name "AWS-RunShellScript" \
-      --parameters '{"commands":["sudo -u ec2-user -i /bin/bash -c \"mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=--max-old-space-size=16384 npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1\""], "executionTimeout":["86400"]}' \
+      --parameters "{\"commands\":[\"sudo -u ec2-user -i /bin/bash -c 'mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1'\"]}" \
       --instance-ids "$instance" \
       --timeout-seconds 86400 >/dev/null 2>&1
-  # Sleep for 50ms before sending the next command
   sleep 0.05
 done
