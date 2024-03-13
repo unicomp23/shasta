9515eae98aa0db2027bba306be8ee453cb39f382
John Davis <john.davis@airtime.com>
2024-03-13 11:14:18 -0700

    revert, c66d374d973525bfef9b478cfd167b8b6c82b01f

--- tools/send-commands.sh
+++ tools/send-commands.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Command for workers
-WORKER_COMMAND="export NODE_OPTIONS='--max-old-space-size=32768' && mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && node --expose-gc -r ts-node/register src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1"
-
-# Command for consumers
-CONSUMER_COMMAND="export NODE_OPTIONS='--max-old-space-size=32768' && mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && node --expose-gc -r ts-node/register src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1"
-
 # Get the list of instance IDs for the workers
 instances=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Role,Values=worker" --query "Reservations[*].Instances[*].InstanceId" --output text --region us-east-1)
 
@@ -15,16 +9,19 @@ instance_array=($instances)
 # Print out the number of instances
 echo "Number of workers: ${#instance_array[@]}"
 
-# Loop over the instances for workers
+# Loop over the instances
 for instance in "${instance_array[@]}"
 do
+  # Log the instance ID
   echo "Sending command to instance: $instance"
+  # Send the command to the instance
   aws ssm send-command \
       --region us-east-1 \
       --document-name "AWS-RunShellScript" \
-      --parameters "{\"commands\":[\"$WORKER_COMMAND\"]}" \
+      --parameters '{"commands":["sudo -u ec2-user -i /bin/bash -c \"mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=--max-old-space-size=16384 npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1\""], "executionTimeout":["86400"]}' \
       --instance-ids "$instance" \
       --timeout-seconds 86400 >/dev/null 2>&1
+  # Sleep for 50ms before moving on to the next instance
   sleep 0.05
 done
 
@@ -37,15 +34,18 @@ instance_array=($instances)
 # Print out the number of instances
 echo "Number of consumers: ${#instance_array[@]}"
 
-# Loop over the instances for consumers
+# Loop over the instances
 for instance in "${instance_array[@]}"
 do
+  # Log the instance ID and command number
   echo "Sending command to instance: $instance"
+  # Send the command to the instance
   aws ssm send-command \
       --region us-east-1 \
       --document-name "AWS-RunShellScript" \
-      --parameters "{\"commands\":[\"$CONSUMER_COMMAND\"]}" \
+      --parameters '{"commands":["sudo -u ec2-user -i /bin/bash -c \"mkdir -p ~/tmp && cd ~/repo/ShastaCdkRepo/shasta && npm i && NODE_OPTIONS=--max-old-space-size=16384 npx ts-node src/lib/pubsub/loadtest.ts > /tmp/log.test.txt 2>&1\""], "executionTimeout":["86400"]}' \
       --instance-ids "$instance" \
       --timeout-seconds 86400 >/dev/null 2>&1
+  # Sleep for 50ms before sending the next command
   sleep 0.05
 done
