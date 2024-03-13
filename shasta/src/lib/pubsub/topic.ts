83a1e729bd2c15a507d2fa2424fea511561e431d
John Davis <john.davis@cantina.ai>
2024-03-13 16:18:52 +0000

    311, more gc testing

--- shasta/src/lib/pubsub/topic.ts
+++ shasta/src/lib/pubsub/topic.ts
@@ -99,7 +99,7 @@ export async function createTopics(topic: string): Promise<void> {
 
 export function generateTopicAndGroupId(): { kafkaTopicLoad: string, groupId: string } {
     //todo const randomTag: string = crypto.randomUUID();
-    const randomTag = "310";
+    const randomTag = "311";
 
     const kafkaTopicLoad = `test_topic_load-${randomTag}`;
     const groupId = `test_group_id-${randomTag}`;
