b7d76b7a9f5ebfd11329e3c80332732981a7937b
John Davis <john.davis@cantina.ai>
2024-03-19 16:52:01 +0000

    319, 16 ec2's in cg

--- shasta/src/lib/pubsub/topic.ts
+++ shasta/src/lib/pubsub/topic.ts
@@ -99,7 +99,7 @@ export async function createTopics(topic: string): Promise<void> {
 
 export function generateTopicAndGroupId(): { kafkaTopicLoad: string, groupId: string } {
     //todo const randomTag: string = crypto.randomUUID();
-    const randomTag = "318";
+    const randomTag = "319";
 
     const kafkaTopicLoad = `test_topic_load-${randomTag}`;
     const groupId = `test_group_id-${randomTag}`;
