f63dcc295271d8b07e5b2ad7135bfc7896e44fb1
John Davis <john.davis@cantina.ai>
2024-03-26 18:42:31 +0000

    wip, docker loadtest run

--- docker/build/docker_run.sh
+++ docker/build/docker_run.sh
@@ -6,15 +6,16 @@ if [ -z "$SHASTA_CDK_ECR_REPO_URI" ]; then
     exit 1
 fi
 
-# Check if an image tag is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <image_tag>"
+# Check if the correct number of arguments is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <image_tag> [command]"
     exit 1
 fi
 
 # Variables
 IMAGE_REPO=$SHASTA_CDK_ECR_REPO_URI # Use the environment variable
 IMAGE_TAG=$1 # Get the first command-line argument
+CMD=${2:-bash} # Get the second command-line argument, default to bash if not provided
 
 # Check if the Docker image with the provided tag exists
 if ! docker image inspect $IMAGE_REPO:$IMAGE_TAG &> /dev/null; then
@@ -22,7 +23,7 @@ if ! docker image inspect $IMAGE_REPO:$IMAGE_TAG &> /dev/null; then
     exit 1
 fi
 
-# Run Docker image with environment variables from the host
+# Run Docker image with environment variables from the host and the specified command
 docker run --rm -it \
   -e CODECOMMIT_REPO_SSH \
   -e CODECOMMIT_STREAM_NAME \
@@ -31,4 +32,4 @@ docker run --rm -it \
   -e BOOTSTRAP_BROKERS \
   -e MULTICAST_STREAM_NAME \
   -e CODECOMMIT_REPO_HTTPS \
-  $IMAGE_REPO:$IMAGE_TAG bash
+  $IMAGE_REPO:$IMAGE_TAG $CMD
