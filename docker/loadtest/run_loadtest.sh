aa6f662997c7c7b8f3d7dc17eccb3e3ff267c3d8
John Davis <john.davis@cantina.ai>
2024-03-26 22:24:08 +0000

    wip, docker load test

---
+++ docker/loadtest/run_loadtest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <n>"
+    exit 1
+fi
+
+N=$1 # Number of instances
+
+# Export N so it's available as an environment variable
+export N
+
+# Use the environment variable in the docker compose command
+docker compose up --scale loadtest=$N
